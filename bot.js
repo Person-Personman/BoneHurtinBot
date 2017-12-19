ac2b7cf463848a185acc2aa9e1934cb22070d9bf
Person-Personman <34670077+Person-Personman@users.noreply.github.com>
2017-12-18 23:16:15 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Eris = require('eris');
+ 
+const bot = new Eris('MzY3MTMyNDQwNTE2NjI0NDA1.DL2-QQ.E6fTcN8MjEKQIByqbOIWAOTla_c');
+ 
+bot.on('ready', () => {
+    console.log('Ready!');
+});
+ 
+bot.on('messageCreate', (msg) => {
+    res = msg.content;
+    str = res.toLowerCase();
+    if(str.indexOf("bone hurting juice") !== -1) {
+        bot.createMessage(msg.channel.id, 'oof owie my bones');
+    }
+    if(str.indexOf(":bhj:") !== -1) {
+        bot.createMessage(msg.channel.id, 'bottoms up!');
+    }
+    if(str.indexOf("oof") !== -1) {
+        bot.createMessage(msg.channel.id, 'owie');
+    }
+    if(str.indexOf("ouch") !== -1) {
+        bot.createMessage(msg.channel.id, 'owie');
+    }
+    if(str.indexOf("owie") !== -1) {
+        bot.createMessage(msg.channel.id, 'mah bones');
+    }
+    if(str.indexOf("pl0x") !== -1) {
+        bot.createMessage(msg.channel.id, 'GET OUT');
+    }
+    if(str.indexOf("plox") !== -1) {
+        bot.createMessage(msg.channel.id, 'GET OUT');
+    }
+});
+ 
+bot.connect();      
---
+++ readme.md
@@ -0,0 +1,6 @@
+To start using BoneHurtinBot V1 (begone th0t edition):
+1. Add the bot to your server: https://discordapp.com/api/oauth2/authorize?client_id=eP-QwaiS2Qm_C4z6VHLtHBuTmaA4Naey&scope=bot&permissions=1
+2. Give message posting permission
+3. Install npm
+4. Open cmd/terminal
+5. Type 'node bot.js'
