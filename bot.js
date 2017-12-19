956fffc87b4af53361646ba3db07267725261d99
Person-Personman <34670077+Person-Personman@users.noreply.github.com>
2017-12-19 00:35:45 -0500

    Update bot.js
    added more gayness
    coated with fish lube
    
    New commands:
    gay - no nigga you gay - @Ian
    lube - make that ｆｉｓｈ ｌｕｂｅ
    fish lube - ｏｈ ｙｅａｈ ｍｏｒｅ ｆｉｓｈ ｌｕｂｅ ｏｈｈｈｈ ｙｅｅｅａａａｈｈｈ
    ｆｉｓｈ ｌｕｂｅ - ｏｈ ｙｅａｈ ｍｏｒｅ ｆｉｓｈ ｌｕｂｅ ｏｈｈｈｈ ｙｅｅｅａａａｈｈｈ

--- bot.js
+++ bot.js
@@ -30,6 +30,15 @@ bot.on('messageCreate', (msg) => {
     if(str.indexOf("plox") !== -1) {
         bot.createMessage(msg.channel.id, 'GET OUT');
     }
+    if(str.indexOf("gay") !== -1) {
+        bot.createMessage(msg.channel.id, 'no nigga you gay - @Ian');
+    }
+    if(str.indexOf("lube") !== -1) {
+        bot.createMessage(msg.channel.id, 'make that ｆｉｓｈ ｌｕｂｅ');
+    }
+    if(str.indexOf("fish lube" || "ｆｉｓｈ ｌｕｂｅ") !== -1) {
+        bot.createMessage(msg.channel.id, 'ｏｈ ｙｅａｈ ｍｏｒｅ ｆｉｓｈ ｌｕｂｅ ｏｈｈｈｈ ｙｅｅｅａａａｈｈｈ');
+    }
 });
  
 bot.connect();      
